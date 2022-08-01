0ad9002906c85c3d4a36d4075ca541aefb1cc43a
odajin <terakawa011@gmail.com>
2022-08-01 09:03:29 +0900

    add spring initializer folder

---
+++ SharedTimer/src/main/java/jp/kobe_u/cs/daikibo/SharedTimer/SharedTimerApplication.java
@@ -0,0 +1,13 @@
+package jp.kobe_u.cs.daikibo.SharedTimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedTimerApplication.class, args);
+	}
+
+}
---
+++ SharedTimer/src/test/java/jp/kobe_u/cs/daikibo/SharedTimer/SharedTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.kobe_u.cs.daikibo.SharedTimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedTimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
